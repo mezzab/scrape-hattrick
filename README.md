c01f33ab75a0eee685c1de5aadb66e810f0f9210
mezzab <marcos32m@gmail.com>
2019-03-08 10:11:57 -0300

    v1.0: basic script

---
+++ README.md
@@ -0,0 +1,12 @@
+# scrape-hattrick
+
+The script searches for a league with more than one bot, so you can move with a friend and share the league.
+
+## Should have installed
+
+- Node
+
+## How to use?
+
+- `yarn install`
+- `yarn scrape`
---
+++ index.js
@@ -0,0 +1,36 @@
+const curl = require("curl");
+const jsdom = require("jsdom");
+
+const primerId = 13220;
+const cantLigas = 256;
+const cantBotsMinima = 2;
+
+const urlBuilder = number =>
+  `https://www93.hattrick.org/World/Series/?LeagueLevelUnitID=${number}`;
+
+for (i = 0; i < cantLigas; i++) {
+  const numeroLiga = i + 1;
+  const url = urlBuilder(primerId + i);
+
+  curl.get(url, null, (err, resp, body) => {
+    if (resp.statusCode == 200) {
+      parseData(body, url, numeroLiga);
+    } else {
+      //some error handling
+      console.log("error while fetching url");
+    }
+  });
+}
+
+function parseData(html, url, i) {
+  const { JSDOM } = jsdom;
+  const dom = new JSDOM(html);
+  const $ = require("jquery")(dom.window);
+  var length = $(".shy").length;
+  if (length >= cantBotsMinima) {
+    console.log(`Numero de liga: V.${i}`);
+    console.log("IdNumber: ", url.slice(url.length - 5));
+    console.log("Cantidad de bots: ", length);
+    console.log(" ");
+  }
+}
